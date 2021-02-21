87dd5ff0bb674968bf90ae04c3931e0fe28b2753
avandvik <anders.vandvik@gmail.com>
2021-02-21 09:45:33 +0100

    Blueprint for ProblemInstance and added Constants file

---
+++ src/main/java/Constants.java
@@ -0,0 +1,6 @@
+public class Constants {
+
+    public static final String VESSEL_FILE = "vessels.json";
+    public static final String INSTALLATION_FILE = "installations.json";
+    public static final String WEATHER_FILE = "weather.json";
+}
--- src/main/java/ProblemInstance.java
+++ src/main/java/ProblemInstance.java
@@ -1,15 +1,83 @@
+import objects.Installation;
+import objects.Order;
+import objects.Vessel;
+
+import java.util.List;
+import java.util.Map;
+
 public class ProblemInstance {
 
-    // CODE HERE: Static fields
+    private final String fileName;
+
+    // Instance objects
+    private static List<Installation> installations;
+    private static List<Order> orders;
+    private static List<Vessel> vessels;
+
+    // Instance info
+    private static int planningPeriodHours;
+    private static int discretizationParam;
+    private static String weatherScenario;
+    private static String installationOrdering;
+
+    // Vessel info
+    private static double minSpeed;
+    private static double designSpeed;
+    private static double maxSpeed;
+    private static double fcDesignSpeed;
+    private static double fcDepot;
+    private static double fcIdling;
+    private static double fcServicing;
+    private static double fuelPrice;
+    private static double spotHourRate;
+    private static double realServiceTimeUnit;
+
+    // Weather
+    private static Map<Integer, Integer> wsToSpeedImpact;
+    private static Map<Integer, Integer> wsToServiceImpact;
+    private static List<Integer> weatherForecast;
 
 
+    public ProblemInstance(String fileName) {
+        this.fileName = fileName;
+    }
 
-    // CODE HERE: Constructor (set file name)
+    public void setUpProblem() {
+        setUpInstallations();
+        setUpOrders();
+        setUpInstanceInfo();
+        setUpVessels();
+        setUpVesselInfo();
+        setUpWeather();
+    }
 
+    private void setUpInstallations() {
+        // Read: INSTALLATION_FILE
+    }
 
+    private void setUpOrders() {
+        // Read: fileName
 
-    // CODE HERE: Read JSON-file and assign values to fields
+    }
 
+    private void setUpVessels() {
+        // Read: VESSEL_FILE and fileName
 
+        // Add return time to each vessel object after they are created
+    }
 
+    private void setUpInstanceInfo() {
+        // Read: fileName
+
+    }
+
+    private void setUpVesselInfo() {
+        // Read: VESSEL_FILE
+
+    }
+
+    private void setUpWeather() {
+        // Read: WEATHER_FILE
+
+    }
 }
