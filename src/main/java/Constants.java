4467aa3f92828f67beed4f17ea1260aba16b0dc6
avandvik <anders.vandvik@gmail.com>
2021-02-21 11:48:11 +0100

    Implemented setUpInstallations

--- src/main/java/Constants.java
+++ src/main/java/Constants.java
@@ -8,4 +8,11 @@ public class Constants {
     public static final String VESSEL_FILE = Constants.PATH_TO_CONSTANT + "vessels.json";
     public static final String INSTALLATION_FILE = Constants.PATH_TO_CONSTANT + "installations.json";
     public static final String WEATHER_FILE = Constants.PATH_TO_CONSTANT + "weather.json";
+
+    public static final String ID_KEY = "id";
+    public static final String OPENING_HOUR_KEY = "opening_hour";
+    public static final String CLOSING_HOUR_KEY = "closing_hour";
+    public static final String LATITUDE_KEY = "latitude";
+    public static final String LONGITUDE_KEY = "longitude";
+    public static final String TYPICAL_DEMAND_KEY = "typical_demand";
 }
--- src/main/java/ProblemInstance.java
+++ src/main/java/ProblemInstance.java
@@ -7,49 +7,47 @@ import org.json.simple.parser.ParseException;
 
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
 public class ProblemInstance {
 
-    private final String fileName;
-    private final String pathToInstanceFile;
+    private static String fileName;
+    private static String pathToInstanceFile;
 
     // Instance objects
-    private static List<Installation> installations;
-    private static List<Order> orders;
-    private static List<Vessel> vessels;
+    public static List<Installation> installations;
+    public static List<Order> orders;
+    public static List<Vessel> vessels;
 
     // Instance info
-    private static int planningPeriodHours;
-    private static int discretizationParam;
-    private static String weatherScenario;
-    private static String installationOrdering;
+    public static int planningPeriodHours;
+    public static int discretizationParam;
+    public static String weatherScenario;
+    public static String installationOrdering;
 
     // Vessel info
-    private static double minSpeed;
-    private static double designSpeed;
-    private static double maxSpeed;
-    private static double fcDesignSpeed;
-    private static double fcDepot;
-    private static double fcIdling;
-    private static double fcServicing;
-    private static double fuelPrice;
-    private static double spotHourRate;
-    private static double realServiceTimeUnit;
+    public static double minSpeed;
+    public static double designSpeed;
+    public static double maxSpeed;
+    public static double fcDesignSpeed;
+    public static double fcDepot;
+    public static double fcIdling;
+    public static double fcServicing;
+    public static double fuelPrice;
+    public static double spotHourRate;
+    public static double realServiceTimeUnit;
 
     // Weather
-    private static Map<Integer, Integer> wsToSpeedImpact;
-    private static Map<Integer, Integer> wsToServiceImpact;
-    private static List<Integer> weatherForecast;
+    public static Map<Integer, Integer> wsToSpeedImpact;
+    public static Map<Integer, Integer> wsToServiceImpact;
+    public static List<Integer> weatherForecast;
 
-
-    public ProblemInstance(String fileName) {
-        this.fileName = fileName;
-        this.pathToInstanceFile = Constants.PATH_TO_INSTANCE + fileName;
-    }
-
-    public void setUpProblem() {
+    public void setUpProblem(String fileName) {
+        ProblemInstance.fileName = fileName;
+        ProblemInstance.pathToInstanceFile = Constants.PATH_TO_INSTANCE + fileName;
         setUpInstallations();
         setUpOrders();
         setUpInstanceInfo();
@@ -58,13 +56,26 @@ public class ProblemInstance {
         setUpWeather();
     }
 
-    private void setUpInstallations() {
-        Object installationObject = getJSONObject(Constants.INSTALLATION_FILE);
-        JSONObject jsonInstallations = (JSONObject) installationObject;
-        System.out.println(jsonInstallations);
+    private static void setUpInstallations() {
+        ProblemInstance.installations = new ArrayList<>();
+        JSONObject jsonInstallations = getJSONObject(Constants.INSTALLATION_FILE);
+        for (Object key : jsonInstallations.keySet()) {
+            JSONObject jsonInstallation = (JSONObject) jsonInstallations.get(key);
+            String name = (String) key;
+            int id = Math.toIntExact((long) jsonInstallation.get(Constants.ID_KEY));
+            int openingHour = Math.toIntExact((long) jsonInstallation.get(Constants.CLOSING_HOUR_KEY));
+            int closingHour = Math.toIntExact((long) jsonInstallation.get(Constants.CLOSING_HOUR_KEY));
+            double latitude = (double) jsonInstallation.get(Constants.LATITUDE_KEY);
+            double longitude = (double) jsonInstallation.get(Constants.LONGITUDE_KEY);
+            double typicalDemand = (double) jsonInstallation.get(Constants.TYPICAL_DEMAND_KEY);
+            Installation installation = new Installation(id, name, openingHour, closingHour, latitude, longitude,
+                    typicalDemand);
+            ProblemInstance.installations.add(installation);
+        }
+        Collections.sort(ProblemInstance.installations);
     }
 
-    private void setUpOrders() {
+    private static void setUpOrders() {
         // Read: fileName
         JSONParser jsonParser = new JSONParser();
 
@@ -72,28 +83,28 @@ public class ProblemInstance {
 
     }
 
-    private void setUpVessels() {
+    private static void setUpVessels() {
         // Read: VESSEL_FILE and fileName
 
         // Add return time to each vessel object after they are created
     }
 
-    private void setUpInstanceInfo() {
+    private static void setUpInstanceInfo() {
         // Read: fileName
 
     }
 
-    private void setUpVesselInfo() {
+    private static void setUpVesselInfo() {
         // Read: VESSEL_FILE
 
     }
 
-    private void setUpWeather() {
+    private static void setUpWeather() {
         // Read: WEATHER_FILE
 
     }
 
-    private JSONObject getJSONObject(String path) {
+    private static JSONObject getJSONObject(String path) {
         JSONParser jsonParser = new JSONParser();
         try (FileReader reader = new FileReader(path)) {
             return (JSONObject) jsonParser.parse(reader);
@@ -104,7 +115,7 @@ public class ProblemInstance {
     }
 
     public static void main(String[] args) {
-        ProblemInstance problemInstance = new ProblemInstance("example.json");
-        problemInstance.setUpInstallations();
+        ProblemInstance.setUpInstallations();
+        System.out.println(ProblemInstance.installations);
     }
 }
