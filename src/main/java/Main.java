ac2f6b6f8d5d06e996251d0d8715a29b2b906cb3
kpulsrud <kapeulsrud@gmail.com>
2021-02-25 08:23:42 +0100

    Fixed requested changes (removed previous constructor in Solution etc.)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,8 @@
-import alns.Solution;
+
 
 public class Main {
 
     public static void main(String[] args) {
-        Solution mySolution = new Solution(3, 13);
-        System.out.println(mySolution);
-
 
     }
 }
--- src/main/java/alns/Solution.java
+++ src/main/java/alns/Solution.java
@@ -25,17 +25,6 @@ public class Solution {
         }
     }
 
-    public Solution(int numberOfVessels, int numberOfOrders) {
-        List<Order> orders = new ArrayList<>();
-        for (int orderId = 0; orderId < numberOfOrders; orderId++) orders.add(new Order(orderId));
-        for (int vesselId = 0; vesselId < numberOfVessels; vesselId++) orderSequences.add(new LinkedList<>());
-
-        while (!orders.isEmpty()) {
-            Order order = orders.remove(this.getRandomIndex(orders.size()));
-            orderSequences.get(this.getRandomVessel(numberOfVessels)).add(order);
-        }
-    }
-
     public int getRandomIndex(int listLength) {
         return random.nextInt(listLength);
     }
