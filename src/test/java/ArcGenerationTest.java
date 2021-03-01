dd11a341254dc48b7ffe317e1a72b583d167b5db
avandvik <anders.vandvik@gmail.com>
2021-03-01 14:23:33 +0100

    Tests for servicingPossible

--- src/test/java/ArcGenerationTest.java
+++ src/test/java/ArcGenerationTest.java
@@ -1,11 +1,14 @@
 import data.Problem;
+import objects.Installation;
 import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import subproblem.ArcGeneration;
+import utils.Helpers;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 public class ArcGenerationTest {
 
@@ -16,7 +19,7 @@ public class ArcGenerationTest {
     @Test
     @DisplayName("Test isReturnPossible")
     public void testIsReturnPossible() {
-        Problem.setUpProblem("example.json");
+        Problem.setUpProblem("testOne.json");
         double distanceOne = 40.0;
         double distanceTwo = 0.0;
         int endTimeOne = 100;
@@ -26,6 +29,33 @@ public class ArcGenerationTest {
         assertFalse(ArcGeneration.isReturnPossible(distanceOne, endTimeTwo));
         assertFalse(ArcGeneration.isReturnPossible(distanceOne, endTimeThree));
         assertTrue(ArcGeneration.isReturnPossible(distanceTwo, endTimeOne));
+    }
+
+    @Test
+    @DisplayName("Test isServicingPossible")
+    public void testIsServicingPossible() {
+        Problem.setUpProblem("testTwo.json");
+        int startRoughWeather = Helpers.getStartTimeOfRoughWeather();
+        int serviceStartTimeOne = 100;
+        int serviceEndTimeOne = 120;
+        int serviceStartTimeTwo = 130;
+        Installation toInstOne = Problem.getInstallation(0);
+        assertTrue(ArcGeneration.isServicingPossible(serviceStartTimeOne, serviceEndTimeOne, toInstOne));
+        assertTrue(ArcGeneration.isServicingPossible(serviceStartTimeTwo, startRoughWeather - 1, toInstOne));
+        assertFalse(ArcGeneration.isServicingPossible(serviceStartTimeTwo, startRoughWeather, toInstOne));
+        assertFalse(ArcGeneration.isServicingPossible(startRoughWeather, startRoughWeather + 10, toInstOne));
+    }
+
+    @Test
+    @DisplayName("Test calculateAverageMaxSpeed")
+    public void testCalculateAverageMaxSpeed() {
+        Problem.setUpProblem("testTwo.json");
+        double distance = 40.0;
+
+        int startSailingTimeOne = 100;
+
+        assertEquals(12.0, ArcGeneration.calculateAverageMaxSpeed(startSailingTimeOne, distance));
+
     }
 }
 
