97136853c49211b4e61d1cd3fe0ca772a22282a6
avandvik <anders.vandvik@gmail.com>
2021-03-01 15:58:47 +0100

    Started om testIsFeasibleLoad 2

--- src/test/java/EvaluatorTest.java
+++ src/test/java/EvaluatorTest.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class EvaluatorTest {
+import alns.Evaluator;
+import alns.Solution;
+import data.Problem;
+import objects.Order;
+import objects.Vessel;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+public class EvaluatorTest {
+
+    @Test
+    public void testIsFeasibleLoad() {
+        Problem.setUpProblem("evaluatorTest.json");
+
+        List<Order> orderSequenceOne = Problem.orders;
+        List<Order> orderSequenceTwo = Problem.orders.subList(0, 3);
+        Vessel vessel = Problem.getVessel(0);
+
+        assertTrue(Evaluator.isFeasibleLoad(orderSequenceOne, vessel));
+        assertTrue(Evaluator.isFeasibleLoad(orderSequenceTwo, vessel));
+    }
 }
