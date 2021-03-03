eb69cb8f0d52a88fe59cd01fa1d8e5ecf5c28cc4
kpulsrud <kapeulsrud@gmail.com>
2021-03-03 12:29:26 +0100

    Continued work on getting feasible insertions. Remains to include possibility of inserting into empty sequences/vessels

--- src/main/java/alns/ConstructionHeuristic.java
+++ src/main/java/alns/ConstructionHeuristic.java
@@ -1,8 +1,10 @@
 package alns;
 
+import data.Problem;
 import objects.Order;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class ConstructionHeuristic {
@@ -10,8 +12,9 @@ public class ConstructionHeuristic {
     public static List<Solution> getAllFeasibleInsertions(Solution solution, Order order) {
         List<Solution> solutions = new ArrayList<>();
         List<List<Order>> orderSequences = solution.getOrderSequences();
+
+        System.out.println(orderSequences);
         for (List<Order> orderSequence : orderSequences) {
-            int vesselNumber = 0;
             if (orderSequence.isEmpty()) {
                 orderSequence.add(order);
                 Solution newSolution = new Solution(orderSequences);
@@ -35,5 +38,25 @@ public class ConstructionHeuristic {
             }
         }
         return solutions;
+    }
+
+    public static void main(String[] args) {
+        Problem.setUpProblem("example.json",false);
+        List<Order> orders = Problem.orders;
+        List<List<Order>> orderSequences = new ArrayList<>();
+        for(int i = 0; i < Problem.getNumberOfVessels(); i++) orderSequences.add(new LinkedList<>());
+
+        for (int i = 0; i < 2; i++) {
+            orderSequences.get(0).add(orders.get(i));
+        }
+        for (int j = 2; j < 5; j++) {
+            orderSequences.get(1).add(orders.get(j));
+        }
+        for (int k = 5; k < 7; k++) {
+            orderSequences.get(2).add(orders.get(k));
+        }
+
+        Solution newSolution = new Solution(orderSequences);
+        List<Solution> insertions = getAllFeasibleInsertions(newSolution,Problem.orders.get(Problem.orders.size()-1));
     }
 }
