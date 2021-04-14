3a57cdac795d84c023196ad14bd0f93ec5251d94
avandvik <anders.vandvik@gmail.com>
2021-04-14 17:21:12 +0200

    Finished one relocate test

--- src/test/java/localsearch/OperatorOneRelocateTest.java
+++ src/test/java/localsearch/OperatorOneRelocateTest.java
@@ -4,8 +4,13 @@ import alns.Objective;
 import alns.Solution;
 import alns.SolutionGenerator;
 import data.Problem;
+import objects.Order;
 import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
+
+import java.util.*;
+
+import static org.junit.Assert.assertEquals;
 
 public class OperatorOneRelocateTest {
 
@@ -16,8 +21,20 @@ public class OperatorOneRelocateTest {
         Objective.initializeCache();
         Solution solution = SolutionGenerator.createSolutionBasicTestData(3, 8);
         Objective.setObjValAndSchedule(solution);
-        System.out.println(solution);
         Solution newSolution = OperatorOneRelocate.oneRelocate(solution);
-        System.out.println(newSolution);
+        Solution expectedSolution = createExpectedSolution();
+        assertEquals(expectedSolution, newSolution);
+    }
+
+    private Solution createExpectedSolution() {
+        List<List<Order>> orderSequences = new ArrayList<>();
+        orderSequences.add(new LinkedList<>(Arrays.asList(Problem.getOrder(2), Problem.getOrder(0),
+                Problem.getOrder(1))));
+        orderSequences.add(new LinkedList<>(Arrays.asList(Problem.getOrder(3), Problem.getOrder(4),
+                Problem.getOrder(6), Problem.getOrder(7), Problem.getOrder(5))));
+        orderSequences.add(new LinkedList<>());
+        Set<Order> postponedOrders = new HashSet<>();
+        Set<Order> unplacedOrders = new HashSet<>();
+        return new Solution(orderSequences, postponedOrders, unplacedOrders);
     }
 }
