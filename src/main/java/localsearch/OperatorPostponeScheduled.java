05ddf926937671abfbcda91570366329ce7b9b60
kpulsrud <kapeulsrud@gmail.com>
2021-04-28 17:29:40 +0200

    Created OperatorPostponeScheduled

--- src/main/java/localsearch/OperatorPostponeScheduled.java
+++ src/main/java/localsearch/OperatorPostponeScheduled.java
@@ -1,4 +1,69 @@
 package localsearch;
 
-public class OperatorPostponeScheduled {
+import alns.Objective;
+import alns.Solution;
+import data.Problem;
+import objects.Installation;
+import objects.Order;
+import utils.Helpers;
+
+import java.util.*;
+
+public class OperatorPostponeScheduled extends Operator {
+
+    private static Map<Integer, Double> vesselToCost;
+    private static double postponedOrdersCost;
+
+    public static Solution postponeScheduled(Solution solution) {
+        initialize(solution);
+        for (int vesselIdx = 0; vesselIdx < Problem.getNumberOfVessels(); vesselIdx++) {
+            double bestCost = calculateOriginalCost(vesselIdx);
+            List<Order> orderSequence = originalSolution.getOrderSequence(vesselIdx);
+            for (Order order : orderSequence) {
+                Solution tempSolution = Helpers.deepCopySolution(originalSolution);
+                tempSolution.getOrderSequence(vesselIdx).remove(order);
+                tempSolution.getPostponedOrders().add(order);
+                bestCost = updateFields(tempSolution, vesselIdx, bestCost);
+            }
+
+        }
+        return newSolution;
+    }
+
+    public static void initialize(Solution solution) {
+        originalSolution = solution;
+        vesselToCost = new HashMap<>();
+        for (int vesselIdx = 0; vesselIdx < Problem.getNumberOfVessels(); vesselIdx++) {
+            List<Order> orderSequence = solution.getOrderSequence(vesselIdx);
+            double cost = orderSequence.isEmpty() ? 0.0 : Objective.runSPLean(orderSequence, vesselIdx); // Cached
+            vesselToCost.put(vesselIdx, cost);
+        }
+        postponedOrdersCost = originalSolution.getPenaltyCosts();
+        newSolution = Helpers.deepCopySolution(solution);
+    }
+
+    private static double calculateOriginalCost(int vesselIdx) {
+        return (vesselToCost.get(vesselIdx) + postponedOrdersCost);
+    }
+
+    private static double calculateNewCost(Solution solution, int vesselIdx) {
+        return (Objective.runSPLean(solution.getOrderSequence(vesselIdx), vesselIdx) + solution.getPenaltyCosts());
+    }
+
+    private static double updateFields(Solution solution, int vesselIdx, double bestCost) {
+        double newCost = calculateNewCost(solution, vesselIdx);
+        if (newCost < bestCost) {
+            bestCost = newCost;
+            List<Order> orderSequence = solution.getOrderSequence(vesselIdx);
+            newSolution.replaceOrderSequence(vesselIdx, orderSequence);
+            replacePostponedOrders(newSolution);
+        }
+        return bestCost;
+    }
+
+    private static void replacePostponedOrders(Solution solution) {
+        newSolution.getPostponedOrders().clear();
+        newSolution.getPostponedOrders().addAll(solution.getPostponedOrders());
+    }
+
 }
