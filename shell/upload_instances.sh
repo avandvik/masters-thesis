4150f93de6ae11f566beafb23ed9fb7b72069920
avandvik <anders.vandvik@gmail.com>
2021-05-14 19:07:22 +0200

    deleted shells

--- shell/upload_instances.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-ssh -i ~/.ssh/id_rsa anderhva@solstorm-login.iot.ntnu.no "rm -rf /home/anderhva/masters-thesis/instances; exit;"
-scp -prq src/main/resources/instances/. anderhva@solstorm-login.iot.ntnu.no:/home/anderhva/masters-thesis/instances/
